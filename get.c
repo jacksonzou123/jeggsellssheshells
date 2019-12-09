25446908339b1063985ca9a76d30de0da0cf5c88
Jackson <jzou00@stuy.edu>
2019-12-08 21:57:06 -0500

    why iS THIS NOT WORKING

--- get.c
+++ get.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <errno.h>
 #include "get.h"
 
 char ** getArgs() {
@@ -13,18 +14,21 @@ char ** getArgs() {
     input[strlen(input) - 1] = '\0';
   }
   char * p = input;
-  char ** list = 0;
+  char **list = calloc(100, sizeof(char*));
   int i = 0;
   while (p) {
     char * hold = strsep(&p, " ");
-    list = realloc(list, (i+1) * sizeof(char*));
+    //list = realloc(list, (i+1) * sizeof(char*));
+    //printf("size of array: %d\n", i+1);
     *(list+i) = hold;
     i++;
   }
-  printf("%d\n",i);
-  i = 0;
-  while (*(list + i)) {
-    printf("[%s]\n", *(list + i++));
-  }
+  *(list+i) = NULL;
+  // printf("%d\n",i);
+  //i = 0;
+  // while (*(list + i)) {
+  //   printf("[%s]\n", *(list + i));
+  //   i++;
+  // }
   return list;
 }
--- main.c
+++ main.c
@@ -4,12 +4,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <errno.h>
 #include "get.h"
 
 int main(int argc, char *argv[]) {
   while (1) {
     printf("GIMME THAT COMMAND: ");
     char ** input = getArgs();
+    int i = 0;
+    while (*(input+i)) {
+      printf("i: %c, str: [%s]\n", i, *(input+i));
+      i++;
+    }
+    *(input+i+1) = NULL;
+    int status = execvp(input[0], input);
+    printf("status: %d , error: %s\n", status, strerror(errno));
+    // int f = fork();
+    // printf("f: %d\n", f);
+    // if (f) {
+    //   //parent
+    //   int * status;
+    //   wait(status);
+    //   printf("parent finished\n");
+    // }
+    // else {
+    //   //kid
+    //   int status = execvp(input[0], input);
+    //   i = 0;
+    //   while (*(input+i)) {
+    //     printf("[%s]\n", *(input+i));
+    //     i++;
+    //   }
+    //   printf("status: %s\n", strerror(errno));
+    //   return 0;
+    // }
     //printf("%s", input);
   }
   return 0;
